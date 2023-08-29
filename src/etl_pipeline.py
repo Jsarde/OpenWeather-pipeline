565c0a3ccdd2c946422edb4f8a568ca27f176c30
Jacopo Sardellini <jacoposardellini@gmail.com>
2023-08-29 14:41:00 +0200

    added HTTP Operator to extract data

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 import os
+import json
 from datetime import datetime,timedelta
+
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
 
 
 load_dotenv()
@@ -27,7 +30,18 @@ dag = DAG(
 
 with dag:
     openweather_api_sensor = HttpSensor(
-        task_id = "is_weather_api_ready",
+        task_id = "is_api_available",
         http_conn_id="openweather_api",
         endpoint=f"/data/2.5/weather?q=Milan&appid={api_key}&units=metrics"
     )
+
+    extract_weather_data = SimpleHttpOperator(
+        task_id="extract_data",
+        http_conn_id="openweather_api",
+        endpoint=f"/data/2.5/weather?q=Milan&appid={api_key}&units=metrics",
+        method="GET",
+        response_filter=lambda r: json.loads(r.text),
+        log_response=True
+    )
+
+    openweather_api_sensor >> extract_weather_data
